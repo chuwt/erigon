6af878cd4e075517d4a9a8882cbca975c410f663
awskii <artem.tsskiy@gmail.com>
2024-06-26 08:54:26 +0100

    save

--- erigon-lib/state/bps_tree.go
+++ erigon-lib/state/bps_tree.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/recsplit/eliasfano32"
+	"github.com/ledgerwatch/log/v3"
 )
 
 // nolint
@@ -111,49 +112,68 @@ type Node struct {
 	prefix []byte
 }
 
-func (b *BpsTree) traverse(g ArchiveGetter, mx [][]Node, n, di, i uint64) {
-	if i >= n {
-		return
-	}
+// func (b *BpsTree) traverse(g ArchiveGetter, mx [][]Node, n, di, i uint64) {
+// 	if i >= n {
+// 		return
+// 	}
 
-	for j := uint64(1); j <= b.M; j += b.M / 2 {
-		ik := i*b.M + j
-		if ik >= n {
-			break
-		}
-		_, k, err := b.keyCmpFunc(nil, ik, g)
-		if err != nil {
-			panic(err)
-		}
-		if k != nil {
-			mx[di] = append(mx[di], Node{off: b.offt.Get(ik), prefix: common.Copy(k), di: ik})
-			//fmt.Printf("d=%d k %x %d\n", di+1, k, offt)
-		}
-		b.traverse(g, mx, n, di, ik)
-	}
-}
+// 	for j := uint64(1); j <= b.M; j += b.M / 2 {
+// 		ik := i*b.M + j
+// 		if ik >= n {
+// 			break
+// 		}
+// 		_, k, err := b.keyCmpFunc(nil, ik, g)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		if k != nil {
+// 			mx[di] = append(mx[di], Node{off: b.offt.Get(ik), prefix: common.Copy(k), di: ik})
+// 			//fmt.Printf("d=%d k %x %d\n", di+1, k, offt)
+// 		}
+// 		b.traverse(g, mx, n, di, ik)
+// 	}
+// }
 
 func (b *BpsTree) WarmUp(kv ArchiveGetter) error {
 	k := b.offt.Count()
-	d := logBase(k, b.M)
+	if k == 0 {
+		return nil
+	}
+	logger := log.New()
 
-	mx := make([][]Node, d+1)
-	_, key, err := b.keyCmpFunc(nil, 0, kv)
-	if err != nil {
-		return err
+	// Value cacheNodesPerM should be tuned along with M. For M=256, cacheNodesPerM=2 is optimal, while 8 could be already too much.
+	// `cacheNodesPerM = 1` means that we put only each parent node into cache, while `cacheNodesPerM = M` means that we put all nodes into cache.
+	cacheNodesPerM := uint64(2) // could increase to put more nodes into cache from one list of childs
+	// d := logBase(k, b.M)
+	mx := make([][]Node, 1) // usually d+1 but for experiments we put them all into flat list
+
+	l := int(0)
+	for ik := uint64(0); ik < k; ik += b.M / cacheNodesPerM {
+		_, key, err := b.keyCmpFunc(nil, ik, kv)
+		if err != nil {
+			return err
+		}
+		if key != nil {
+			mx[l] = append(mx[l], Node{off: b.offt.Get(ik), prefix: common.Copy(key), di: ik})
+			// l++
+			// if l == len(mx) {
+			// 	l = 0
+			// }
+		}
 	}
-	if key != nil {
-		mx[0] = append(mx[0], Node{off: b.offt.Get(0), prefix: common.Copy(key)})
-		//fmt.Printf("d=%d k %x %d\n", di, k, offt)
-	}
-	b.traverse(kv, mx, k, 0, 0)
 
 	if b.trace {
+		c := 0
 		for i := 0; i < len(mx); i++ {
+			c += len(mx[i])
+			ll := make([]uint64, len(mx[i]))
 			for j := 0; j < len(mx[i]); j++ {
-				fmt.Printf("mx[%d][%d] %x %d %d\n", i, j, mx[i][j].prefix, mx[i][j].off, mx[i][j].di)
+				ll[j] = mx[i][j].di
+				// fmt.Printf("mx[%d][%d] %x %d %d\n", i, j, mx[i][j].prefix, mx[i][j].off, mx[i][j].di)
 			}
+			logger.Debug("WarmUp", "file", kv.FileName(), "depth", i, "offsets", len(mx[i]), "v", fmt.Sprintf("%v", ll))
 		}
+		logger.Debug("WarmUp", "M", b.M, "depth", len(mx), "total offsets", k, "cached", c, "percentage", float64(c)/float64(k)*100)
 	}
 	b.mx = mx
 	return nil
