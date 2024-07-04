2c839601a790cc7a1c673912ba51e44e167df42d
chuwt <weitaochu@gmail.com>
2024-07-04 17:37:48 +0800

    feat: token contract

--- turbo/transactions/token_contract.sol
+++ turbo/transactions/token_contract.sol
@@ -43,4 +43,90 @@ contract BalanceChecker {
         }
         return userBalances;
     }
+
+    function bytes32ToString(bytes32 _bytes32) internal pure returns (string memory) {
+        uint8 i = 0;
+        while(i < 32 && _bytes32[i] != 0) {
+            i++;
+        }
+        bytes memory bytesArray = new bytes(i);
+        for (uint8 j = 0; j < i; j++) {
+            bytesArray[j] = _bytes32[j];
+        }
+        return string(bytesArray);
+    }
+
+    function tokenInfo(address[] memory tokens) view public returns (string[] memory names, string[] memory symbols, uint256[] memory decimals, bool[] memory hasDecimals, uint256[] memory totalSupplies, bool[] memory hasTotalSupply) {
+        names = new string[](tokens.length);
+        symbols = new string[](tokens.length);
+        decimals = new uint256[](tokens.length);
+        hasDecimals = new bool[](tokens.length);
+        totalSupplies = new uint256[](tokens.length);
+        hasTotalSupply = new bool[](tokens.length);
+
+        bool success;
+        bytes memory data;
+        for (uint i = 0; i < tokens.length; i++) {
+            address token = tokens[i];
+
+            (success, data) = token.staticcall(abi.encodeWithSignature("name()"));
+            if (success) {
+                if (data.length == 32) {
+                    bytes32 nameBytes32;
+                    assembly {
+                        nameBytes32 := mload(add(data, 32))
+                    }
+                    names[i] = bytes32ToString(nameBytes32);
+                } else if (data.length > 32) {
+                    names[i] = abi.decode(data, (string));
+                } else {
+                    names[i] = "";
+                }
+            } else {
+                names[i] = "certik-false";
+            }
+            (success, data) = token.staticcall(abi.encodeWithSignature("symbol()"));
+            if (success) {
+                if (data.length == 32) {
+                    bytes32 symbolBytes32;
+                    assembly {
+                        symbolBytes32 := mload(add(data, 32))
+                    }
+                    symbols[i] = bytes32ToString(symbolBytes32);
+                } else if (data.length > 32) {
+                    symbols[i] = abi.decode(data, (string));
+                } else {
+                    symbols[i] = "";
+                }
+            } else {
+                symbols[i] = "certik-false";
+            }
+
+            (success, data) = token.staticcall(abi.encodeWithSignature("decimals()"));
+            if (success) {
+                hasDecimals[i] = true;
+                if (data.length == 0) {
+                    decimals[i] = 0;
+                } else {
+                    decimals[i] = abi.decode(data, (uint256));
+                }
+            } else {
+                hasDecimals[i] = false;
+                decimals[i] = 0;
+            }
+            (success, data) = token.staticcall(abi.encodeWithSignature("totalSupply()"));
+            if (success) {
+                hasTotalSupply[i] = true;
+                if (data.length == 0) {
+                    totalSupplies[i] = 0;
+                } else {
+                    totalSupplies[i] = abi.decode(data, (uint256));
+                }
+            } else {
+                hasTotalSupply[i] = false;
+                totalSupplies[i] = 0;
+            }
+        }
+        return (names, symbols, decimals, hasDecimals, totalSupplies, hasTotalSupply);
+    }
 }
